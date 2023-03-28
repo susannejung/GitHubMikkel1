788e7c85995044715aefc27317e9803dcd30d563
SUHA <suha@zealand.dk>
2023-03-28 10:15:21 +0200

    Commit branch1_1

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world! branch 1");
         System.out.println("Hello world! branch 1_1");
+        System.out.println("Hello world! Ændring af branch1");
     }
 }
