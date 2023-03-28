d4721c54b7cd7e0f146822220fe7b30f2f0bb8b7
SUHA <suha@zealand.dk>
2023-03-28 10:09:58 +0200

    Commit 2

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+        System.out.println("Hello world! branch 1");
+        System.out.println("Hello world! branch 1_1");
     }
 }
